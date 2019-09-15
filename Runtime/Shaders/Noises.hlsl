6896358e429b27ed382b2e2b7d84ef0ef99bb5f1
Antoine Lelievre <a.lelievre@infotech.fr>
2019-09-15 21:15:34 +0200

    Begin FBM node

--- Runtime/Shaders/Noises.hlsl
+++ Runtime/Shaders/Noises.hlsl
@@ -96,6 +96,53 @@ float3 Generate##NAME##CurlNoise(float3 coordinate, float frequency, int octaveC
         (total[1].x - total[0].y)) / totalAmplitude; \
 }
 
+// TODO: make the rotation a parameter
+const float2x2 rMatrix = float2x2( 0.80,  0.60, -0.60,  0.80 );
+
+#define FBM4_TEMPLATE(NAME, COORDINATE_TYPE, FUNC) \
+float Generate##NAME##_FBM4(COORDINATE_TYPE p) \
+{ \
+    float f = 0.0; \
+ \
+    f += 0.5000 * (-1.0 + 2.0 * FUNC(p)); p = mul(rMatrix, p) * 2.02; \
+    f += 0.2500 * (-1.0 + 2.0 * FUNC(p)); p = mul(rMatrix, p) * 2.03; \
+    f += 0.1250 * (-1.0 + 2.0 * FUNC(p)); p = mul(rMatrix, p) * 2.01; \
+    f += 0.0625 * (-1.0 + 2.0 * FUNC(p)); \
+ \
+    return f / 0.9375; \
+} \
+ \
+float2 Generate##NAME##_FBM4_2(float2 p) \
+{ \
+    return float2( \
+        Generate##NAME##_FBM4(p + -5.92), \
+        Generate##NAME##_FBM4(p + 3.88) \
+    ); \
+}
+
+#define FBM6_TEMPLATE(NAME, COORDINATE_TYPE, FUNC) \
+float Generate##NAME##_FBM6(COORDINATE_TYPE p) \
+{ \
+    float f = 0.0; \
+ \
+    f += 0.500000 * FUNC(p); p = mul(rMatrix, p) * 2.02; \
+    f += 0.250000 * FUNC(p); p = mul(rMatrix, p) * 2.03; \
+    f += 0.125000 * FUNC(p); p = mul(rMatrix, p) * 2.01; \
+    f += 0.062500 * FUNC(p); p = mul(rMatrix, p) * 2.04; \
+    f += 0.031250 * FUNC(p); p = mul(rMatrix, p) * 2.01; \
+    f += 0.015625 * FUNC(p); \
+ \
+    return f / 0.96875; \
+} \
+COORDINATE_TYPE Generate##NAME##_FBM6_2(COORDINATE_TYPE p) \
+{ \
+    return COORDINATE_TYPE( \
+        Generate##NAME##_FBM6(p + 1.99), \
+        Generate##NAME##_FBM6(p + -4.18) \
+    ); \
+}
+
+
 // Perlin:
 
 float4 Interpolation_C2_InterpAndDeriv(float2 x) { return x.xyxy * x.xyxy * (x.xyxy * (x.xyxy * (x.xyxy * float2(6.0f, 0.0f).xxyy + float2(-15.0f, 30.0f).xxyy) + float2(10.0f, -60.0f).xxyy) + float2(0.0f, 30.0f).xxyy); }
@@ -364,12 +411,38 @@ float3 GenerateRidgedCellularNoise2D(float2 coordinate)
     return r * 2 - 1.0;
 }
 
+float4 GenerateRidgedCellularNoise3D(float3 coordinate)
+{
+    float4 r = GenerateCellularNoise3D(coordinate);
+    return r * 2 - 1.0;
+}
+
 NOISE_TEMPLATE(Cellular2D, float2, float3, GenerateCellularNoise2D);
 NOISE_TEMPLATE(Cellular3D, float3, float4, GenerateCellularNoise3D);
 RIDGED_NOISE_TEMPLATE(Cellular2D, float2, float3, GenerateRidgedCellularNoise2D);
-RIDGED_NOISE_TEMPLATE(Cellular3D, float3, float4, GenerateCellularNoise3D);
+RIDGED_NOISE_TEMPLATE(Cellular3D, float3, float4, GenerateRidgedCellularNoise3D);
 
 CURL_NOISE_2D_TEMPLATE(Cellular2D, GenerateCellularNoise2D);
 CURL_NOISE_3D_TEMPLATE(Cellular3D, GenerateCellularNoise2D);
+
+// FBMs:
+
+float fbmPerlinNoise2D(float2 c) { return perlinNoise2D(c).x; }
+
+FBM4_TEMPLATE(Perlin2D, float2, fbmPerlinNoise2D);
+FBM6_TEMPLATE(Perlin2D, float2, perlinNoise2D);
+
+float GeneratePerlin2D_FBM(float2 coordinate)
+{
+    float2 uvSet1 = 0.5 * GeneratePerlin2D_FBM4_2(coordinate) + 0.5;
+
+    float2 n = GeneratePerlin2D_FBM6_2( 4.0*uvSet1 );
+
+    float2 p = coordinate + 2.0*n;
+
+    float f = 0.5 + 0.5 * GeneratePerlin2D_FBM4( 2.0*p );
+
+    return f;
+}
 
 #endif
