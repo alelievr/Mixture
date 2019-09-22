fb3a1ab2773ce4cb14da497943c7d089b1c74ee2
Antoine Lelievre <a.lelievre@infotech.fr>
2019-09-22 20:38:20 +0200

    Begin 3d tileable perlin noise

--- Runtime/Shaders/Noises.hlsl
+++ Runtime/Shaders/Noises.hlsl
@@ -449,10 +449,8 @@ float4 permute(float4 x)
     return mod289(((x * 34) + 1) * x);
 }
 
-float2 fade(float2 t)
-{
-    return (t * t * t) * (t * (t * 6 - 15) + 10);
-}
+float2 fade(float2 t) { return (t * t * t) * (t * (t * 6 - 15) + 10); }
+float3 fade(float3 t) { return (t * t * t) * (t * (t * 6 - 15) + 10); }
 
 float tiledPerlinNoise2D(float2 coordinate, float2 period)
 {
@@ -470,7 +468,7 @@ float tiledPerlinNoise2D(float2 coordinate, float2 period)
     float4 gx = 2 * frac(i / float(41)) - float(1);
     float4 gy = abs(gx) - float(0.5);
     float4 tx = floor(gx + float(0.5));
-    // gx = gx - tx;
+    gx = gx - tx;
 
     float2 g00 = float2(gx.x, gy.x);
     float2 g10 = float2(gx.y, gy.y);
@@ -496,7 +494,71 @@ float tiledPerlinNoise2D(float2 coordinate, float2 period)
 
 float tiledPerlinNoise3D(float3 coordinate, float3 period)
 {
-    return 0;
+    float3 Pi0 = fmod(floor(coordinate), period); // Integer part, modulo period
+    float3 Pi1 = fmod(Pi0 + 1, period); // Integer part + 1, fmod period
+    Pi0 = fmod(Pi0, 289);
+    Pi1 = fmod(Pi1, 289);
+    float3 Pf0 = frac(coordinate); // Fractional part for interpolation
+    float3 Pf1 = Pf0 - 1; // Fractional part - 1.0
+    float4 ix = float4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+    float4 iy = float4(Pi0.y, Pi0.y, Pi1.y, Pi1.y);
+    float4 iz0 = Pi0.z;
+    float4 iz1 = Pi1.z;
+
+    float4 ixy = permute(permute(ix) + iy);
+    float4 ixy0 = permute(ixy + iz0);
+    float4 ixy1 = permute(ixy + iz1);
+
+    float4 gx0 = ixy0 / 7;
+    float4 gy0 = frac(floor(gx0) / 7) - 0.5;
+    gx0 = frac(gx0);
+    float4 gz0 = 0.5 - abs(gx0) - abs(gy0);
+    float4 sz0 = step(gz0, 0);
+    gx0 -= sz0 * (step(0, gx0) - 0.5);
+    gy0 -= sz0 * (step(0, gy0) - 0.5);
+
+    float4 gx1 = ixy1 / 7;
+    float4 gy1 = frac(floor(gx1) / 7) - 0.5;
+    gx1 = frac(gx1);
+    float4 gz1 = 0.5 - abs(gx1) - abs(gy1);
+    float4 sz1 = step(gz1, 0);
+    gx1 -= sz1 * (step(0, gx1) - 0.5);
+    gy1 -= sz1 * (step(0, gy1) - 0.5);
+
+    float3 g000 = float3(gx0.x, gy0.x, gz0.x);
+    float3 g100 = float3(gx0.y, gy0.y, gz0.y);
+    float3 g010 = float3(gx0.z, gy0.z, gz0.z);
+    float3 g110 = float3(gx0.w, gy0.w, gz0.w);
+    float3 g001 = float3(gx1.x, gy1.x, gz1.x);
+    float3 g101 = float3(gx1.y, gy1.y, gz1.y);
+    float3 g011 = float3(gx1.z, gy1.z, gz1.z);
+    float3 g111 = float3(gx1.w, gy1.w, gz1.w);
+
+    float4 norm0 = taylorInvSqrt(float4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+    g000 *= norm0.x;
+    g010 *= norm0.y;
+    g100 *= norm0.z;
+    g110 *= norm0.w;
+    float4 norm1 = taylorInvSqrt(float4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+    g001 *= norm1.x;
+    g011 *= norm1.y;
+    g101 *= norm1.z;
+    g111 *= norm1.w;
+
+    float n000 = dot(g000, Pf0);
+    float n100 = dot(g100, float3(Pf1.x, Pf0.y, Pf0.z));
+    float n010 = dot(g010, float3(Pf0.x, Pf1.y, Pf0.z));
+    float n110 = dot(g110, float3(Pf1.x, Pf1.y, Pf0.z));
+    float n001 = dot(g001, float3(Pf0.x, Pf0.y, Pf1.z));
+    float n101 = dot(g101, float3(Pf1.x, Pf0.y, Pf1.z));
+    float n011 = dot(g011, float3(Pf0.x, Pf1.y, Pf1.z));
+    float n111 = dot(g111, Pf1);
+
+    float3 fade_xyz = fade(Pf0);
+    float4 n_z = lerp(float4(n000, n100, n010, n110), float4(n001, n101, n011, n111), fade_xyz.z);
+    float2 n_yz = lerp(float2(n_z.x, n_z.y), float2(n_z.z, n_z.w), fade_xyz.y);
+    float2 n_xyz = lerp(n_yz.x, n_yz.y, fade_xyz.x);
+    return 2.2 * n_xyz;
 }
 
 #ifdef _TILINGMODE_TILED
