d3182a84c7400c1c7d95e5b3b820b35cceca7bd7
Antoine Lelievre <a.lelievre@infotech.fr>
2020-04-02 13:02:44 +0200

    Subgraph test

---
+++ Assets/T/Editor/View.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GraphProcessor;
+using UnityEditor.Experimental.GraphView;
+
+namespace Mixture
+{
+    [NodeCustomEditor(typeof(Suibgraph))]
+	public class SuibgraphView : MixtureNodeView
+    {
+        public override void Enable()
+        {
+            var view = new BaseGraphView(MixtureGraphWindow.FindObjectOfType<MixtureGraphWindow>());
+            var mixtureGraph = new MixtureGraph();
+            mixtureGraph.AddNode(BaseNode.CreateFromType<TextureNode>(Vector2.zero));
+            view.Initialize(mixtureGraph);
+            controlsContainer.style.width = 500;
+            controlsContainer.style.height = 300;
+            controlsContainer.Add(view);
+        }
+    }
+}
---
+++ Assets/T/Runtime/Suibgraph.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GraphProcessor;
+using System.Linq;
+
+namespace Mixture
+{
+	[System.Serializable, NodeMenuItem("Custom/Suibgraph")]
+	public class Suibgraph : MixtureNode
+	{
+		[Input]
+		public Texture2D input;
+		[Output]
+		public Texture2D output;
+
+		public override string name => "Suibgraph";
+	}
+}
