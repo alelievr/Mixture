7db3839027dcffd50865561b1ee8b333304a7b16
Cyril <jovercyril@gmail.com>
2020-06-25 00:19:39 +0200

    Show inspector in integer node

--- Packages/com.alelievr.mixture/Runtime/Nodes/Constants/IntegerNode.cs
+++ Packages/com.alelievr.mixture/Runtime/Nodes/Constants/IntegerNode.cs
@@ -9,11 +9,12 @@ namespace Mixture
 	[System.Serializable, NodeMenuItem("Constants/Integer")]
 	public class IntegerNode : MixtureNode
 	{
-		[Output(name = "Integer")]
+		[Output(name = "Integer"),SerializeField]
 		public int Int = 1;
 
 		public override bool 	hasSettings => false;
 		public override string	name => "Integer";
 		public override float	nodeWidth => 200;
+		public override bool showDefaultInspector => true;
 	}
 }
