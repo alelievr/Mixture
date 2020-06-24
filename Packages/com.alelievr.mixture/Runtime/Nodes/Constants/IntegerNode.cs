110c13395307bc23e0de9007d773e473906690cb
Cyril <jovercyril@gmail.com>
2020-06-25 00:03:53 +0200

    Add integer node

---
+++ Packages/com.alelievr.mixture/Runtime/Nodes/Constants/IntegerNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GraphProcessor;
+using System;
+
+namespace Mixture
+{
+	[System.Serializable, NodeMenuItem("Constants/Integer")]
+	public class IntegerNode : MixtureNode
+	{
+		[Output(name = "Integer")]
+		public int Int = 1;
+
+		public override bool 	hasSettings => false;
+		public override string	name => "Integer";
+		public override float	nodeWidth => 200;
+	}
+}
