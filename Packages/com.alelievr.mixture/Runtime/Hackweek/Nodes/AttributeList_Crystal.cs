b79564d5a4a716a37608fd85d3e708a8f06007b8
Cyril <jovercyril@gmail.com>
2020-06-25 03:02:57 +0200

    Adding node specific to crystal distibution

--- Packages/com.alelievr.mixture/Runtime/Hackweek/Nodes/AttributeList_Crystal.cs
+++ Packages/com.alelievr.mixture/Runtime/Hackweek/Nodes/AttributeList_Crystal.cs
@@ -8,13 +8,13 @@ using Net3dBool;
 
 namespace Mixture
 {
-	[System.Serializable, NodeMenuItem("Attribute List Random")]
-	public class AttributeList_Random : MixtureNode
+	[System.Serializable, NodeMenuItem("Attribute List Crystal")]
+	public class AttributeList_Crystal : MixtureNode
     {
         [Output("Output")]
         public MixtureAttributeList attributes = new MixtureAttributeList();
 
-		public override string	name => "Attribute List Random";
+		public override string	name => "Attribute List Crystal";
 
 		public override bool    hasPreview => false;
 		public override bool    showDefaultInspector => true;
@@ -24,10 +24,14 @@ namespace Mixture
         [Input("Seed")]
         public int seed = 0;
         public float dirRange = 1;
+        public float squashY = 1;
         public Vector3 scaleRange = new Vector3(0,0,0);
         public float uniformscaleRange = 0;
         public Vector3 posRange = new Vector3(0, 0, 0);
+        public float posFromNormRange = 0;
         public float scaleFromDir = 0;
+        public float scaleFromDirMin = 0;
+        public float scaleFromDirShape = 1;
 
 
         protected override void Enable()
@@ -70,15 +74,20 @@ namespace Mixture
 
                 Vector3 normal = new Vector3(Random.Range(-dirRange, dirRange), Random.Range(0, dirRange), Random.Range(-dirRange, dirRange));
                 normal += new Vector3(0, 0.5f, 0);
+                normal = Vector3.Scale(normal,new Vector3(1, squashY, 1));
                 normal.Normalize();
                 //Debug.Log(normal);
 
                 Vector3 scale = new Vector3(Random.Range(-scaleRange.x, scaleRange.x), Random.Range(-scaleRange.y, scaleRange.y), Random.Range(-scaleRange.z, scaleRange.z));
                 scale += Vector3.one;
                 scale *= Random.Range(1 - uniformscaleRange, 1 + uniformscaleRange);
-
+                float scaleFromDirValue = Vector3.Dot(normal, Vector3.up);
+                scaleFromDirValue = Mathf.Pow(scaleFromDirValue, scaleFromDirShape);
+                scaleFromDirValue = Mathf.Max(scaleFromDirValue, scaleFromDirMin);
+                scale *= Mathf.Lerp(1, scaleFromDirValue, scaleFromDir);
 
                 Vector3 pos = new Vector3(Random.Range(-posRange.x, posRange.x), Random.Range(-posRange.y, posRange.y), Random.Range(-posRange.z, posRange.z));
+                pos += Random.Range(0, posFromNormRange) * Vector3.Scale(normal, new Vector3(1,0,1)); 
 
                 attributes.Add(new MixtureAttribute{
                     {"position", pos},
