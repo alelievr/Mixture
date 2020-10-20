0a52f2e0a330c23719c749f6ea150cee32e16293
Antoine Lelièvre <antoinel@unity3d.com>
2020-10-21 01:29:53 +0200

    Improved a lot the curve node

--- Packages/com.alelievr.mixture/Editor/Views/CurveNodeView.cs
+++ Packages/com.alelievr.mixture/Editor/Views/CurveNodeView.cs
@@ -3,6 +3,7 @@ using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine.UIElements;
 using GraphProcessor;
+using System;
 
 namespace Mixture
 {
@@ -16,19 +17,87 @@ namespace Mixture
 			base.Enable(fromInspector);
 			curveNode = nodeTarget as CurveNode;
 
-			var gradientField = new CurveField() {
-                value = curveNode.curve
-			};
-            gradientField.style.height = 128;
+			styleSheets.Add(Resources.Load<StyleSheet>("MixtureCurveColors"));
 
-			gradientField.RegisterValueChangedCallback(e => {
-				owner.RegisterCompleteObjectUndo("Updated Curve");
-				curveNode.curve = (AnimationCurve)e.newValue;
-				NotifyNodeChanged();
+			var modeField = new EnumField("Mode", curveNode.mode);
+
+			controlsContainer.Add(modeField as VisualElement);
+
+			var rCurve = CreateCurveField(() => curveNode.redCurve, c => curveNode.redCurve = c);
+			var gCurve = CreateCurveField(() => curveNode.greenCurve, c => curveNode.greenCurve = c);
+			var bCurve = CreateCurveField(() => curveNode.blueCurve, c => curveNode.blueCurve = c);
+			var aCurve = CreateCurveField(() => curveNode.alphaCurve, c => curveNode.alphaCurve = c);
+
+			rCurve.AddToClassList("red");
+			gCurve.AddToClassList("green");
+			bCurve.AddToClassList("blue");
+			aCurve.AddToClassList("white");
+
+			controlsContainer.Add(rCurve);
+			controlsContainer.Add(gCurve);
+			controlsContainer.Add(bCurve);
+			controlsContainer.Add(aCurve);
+
+			modeField.RegisterValueChangedCallback(e => {
+				curveNode.mode = (CurveNode.CurveOutputMode)e.newValue;
+				owner.RegisterCompleteObjectUndo("Change Curve Mode");
+				UpdateVisibleCurves();
                 curveNode.UpdateTexture();
+				NotifyNodeChanged();
+			});
+			UpdateVisibleCurves();
+
+			var p = new CustomStyleProperty<Color>("--unity-curve-color");
+
+			void UpdateVisibleCurves()
+			{
+				rCurve.style.display = DisplayStyle.None;
+				gCurve.style.display = DisplayStyle.None;
+				bCurve.style.display = DisplayStyle.None;
+				aCurve.style.display = DisplayStyle.None;
+
+				switch (curveNode.mode)
+				{
+					case CurveNode.CurveOutputMode.RRRR:
+					case CurveNode.CurveOutputMode.R:
+						rCurve.style.display = DisplayStyle.Flex;
+						break;
+					case CurveNode.CurveOutputMode.RG:
+						rCurve.style.display = DisplayStyle.Flex;
+						gCurve.style.display = DisplayStyle.Flex;
+						break;
+					case CurveNode.CurveOutputMode.RGB:
+						rCurve.style.display = DisplayStyle.Flex;
+						gCurve.style.display = DisplayStyle.Flex;
+						bCurve.style.display = DisplayStyle.Flex;
+						break;
+					case CurveNode.CurveOutputMode.RGBA:
+						rCurve.style.display = DisplayStyle.Flex;
+						gCurve.style.display = DisplayStyle.Flex;
+						bCurve.style.display = DisplayStyle.Flex;
+						aCurve.style.display = DisplayStyle.Flex;
+						break;
+				}
+			}
+		}
+
+
+		CurveField CreateCurveField(Func<AnimationCurve> getter, Action<AnimationCurve> setter)
+		{
+			var curveField = new CurveField() {
+                value = getter(),
+				renderMode = CurveField.RenderMode.Mesh,
+			};
+            curveField.style.height = 128;
+
+			curveField.RegisterValueChangedCallback(e => {
+				owner.RegisterCompleteObjectUndo("Updated Curve");
+				setter((AnimationCurve)e.newValue);
+                curveNode.UpdateTexture();
+				NotifyNodeChanged();
 			});
 
-			controlsContainer.Add(gradientField);
+			return curveField;
 		}
 	}
 }
