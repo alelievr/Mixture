abe25a5733b4e63741ad8253e7c664059f5b14c1
Antoine Lelièvre <antoinel@unity3d.com>
2020-11-02 23:54:48 +0100

    Fixed time node modifying the graph

--- Packages/com.alelievr.mixture/Runtime/Nodes/Utils/TimeNode.cs
+++ Packages/com.alelievr.mixture/Runtime/Nodes/Utils/TimeNode.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using GraphProcessor;
-using System.Linq;
+using System;
 using UnityEngine.Rendering;
 
 namespace Mixture
@@ -16,19 +14,19 @@ This node can be used in a realtime graph for time based effects.
 	[System.Serializable, NodeMenuItem("Utils/Time")]
 	public class TimeNode : MixtureNode
 	{
-		[Output(name = "Time")]
+		[Output(name = "Time"), NonSerialized]
 		public float				time;
 
-		[Output(name = "Sin Time")]
+		[Output(name = "Sin Time"), NonSerialized]
 		public float				sinTime;
 
-		[Output(name = "Cos Time")]
+		[Output(name = "Cos Time"), NonSerialized]
 		public float				cosTime;
 
-		[Output(name = "Delta Time")]
+		[Output(name = "Delta Time"), NonSerialized]
 		public float				deltaTime;
 
-		[Output(name = "Frame Count")]
+		[Output(name = "Frame Count"), NonSerialized]
 		public float				frameCount;
 
 		public override bool 		hasSettings => false;
