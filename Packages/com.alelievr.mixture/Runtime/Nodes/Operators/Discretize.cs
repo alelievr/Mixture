699bfc6ea46f16dd03cc730a2cf25a063d103b11
Antoine Lelièvre <antoinel@unity3d.com>
2021-02-03 01:41:58 +0100

    Added discretize doc

--- Packages/com.alelievr.mixture/Runtime/Nodes/Operators/Discretize.cs
+++ Packages/com.alelievr.mixture/Runtime/Nodes/Operators/Discretize.cs
@@ -6,6 +6,13 @@ using System.Linq;
 
 namespace Mixture
 {
+	[Documentation(@"
+Discretize an image, rounding it's color components to make the specified number of steps in the image.
+This node can also be used to make a posterize effect.
+
+By default the input values are considered to be between 0 and 1, you can change these values in the node inspector to adapt the effect to your input data.
+")]
+
 	[System.Serializable, NodeMenuItem("Operators/Discretize")]
 	public class Discretize : FixedShaderNode
 	{
@@ -17,12 +24,5 @@ namespace Mixture
 
 		// Enumerate the list of material properties that you don't want to be turned into a connectable port.
 		protected override IEnumerable<string> filteredOutProperties => new string[]{};
-
-		// Override this if you node is not compatible with all dimensions
-		// public override List<OutputDimension> supportedDimensions => new List<OutputDimension>() {
-		// 	OutputDimension.Texture2D,
-		// 	OutputDimension.Texture3D,
-		// 	OutputDimension.CubeMap,
-		// };
 	}
 }
