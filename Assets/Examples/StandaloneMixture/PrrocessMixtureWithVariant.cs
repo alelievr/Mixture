b08fbe13345b6fde79d64a82fb5de26e49a5a4bb
Antoine Lelievre <antoinel@unity3d.com>
2021-04-22 01:02:09 +0200

    Added mixture variant docv

---
+++ Assets/Examples/StandaloneMixture/PrrocessMixtureWithVariant.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.UI;
+using Mixture;
+using System.IO;
+
+public class ProcessMixtureWithVariant : MonoBehaviour
+{
+    public Texture graphTexture;
+
+    public Texture2D source;
+    public Texture2D target;
+
+    public RawImage         image;
+
+    void Start()
+    {
+        var graph = MixtureDatabase.GetGraphFromTexture(graphTexture);
+
+        var variant = ScriptableObject.CreateInstance<MixtureVariant>();
+        variant.SetParent(graph);
+
+        variant.SetParameterValue("Source", source);
+        variant.SetParameterValue("Target", target);
+
+        variant.ProcessGraphWithOverrides();
+
+        // Create the destination texture
+        var settings = graph.outputNode.rtSettings;
+        Texture2D destination = new Texture2D(
+            settings.GetWidth(graph),
+            settings.GetHeight(graph),
+            settings.GetGraphicsFormat(graph),
+            TextureCreationFlags.None
+        );
+
+        // Readback the result
+        graph.ReadbackMainTexture(destination);
+
+        // Write the file at the target destination
+        var bytes = ImageConversion.EncodeToPNG(destination);
+        File.WriteAllBytes("C:\\Users\\Antoine Lelievre\\test.png", bytes);
+    }
+}
