a0bbf762ce1eef3720e87c973c560a84266214a6
Antoine Lelievre <antoinel@unity3d.com>
2021-06-18 22:44:11 +0200

    Fix texture  sample node path

--- Packages/com.alelievr.mixture/Runtime/Nodes/FixedShaderNodes/TextureSampleNode.cs
+++ Packages/com.alelievr.mixture/Runtime/Nodes/FixedShaderNodes/TextureSampleNode.cs
@@ -7,7 +7,7 @@ namespace Mixture
 Sample a texture. Note that you can use a custom UV texture as well.
 ")]
 
-	[System.Serializable, NodeMenuItem("Utils/Texture Sample")]
+	[System.Serializable, NodeMenuItem("Textures/Texture Sample")]
 	public class TextureSampleNode : FixedShaderNode
 	{
 		public override string name => "Texture Sample";
