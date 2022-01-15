b5f8a99f4c13d7da61a3f3329b649e071b9d4f1a
Thomas ICHÉ <4037271+peeweek@users.noreply.github.com>
2022-01-15 21:37:39 +0100

    UV Polar Node

---
+++ Packages/com.alelievr.mixture/Runtime/Nodes/FixedShaderNodes/UVPolarNode.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GraphProcessor;
+using System.Linq;
+
+namespace Mixture
+{
+	[Documentation(@"
+Constant Polar UV. Note that for texture 2D, the z coordinate is set to 0.5.
+")]
+
+	[System.Serializable, NodeMenuItem("Constants/UV Polar")]
+	public class UVPolarNode : FixedShaderNode
+	{
+		public override string name => "UV Polar";
+
+		public override string shaderName => "Hidden/Mixture/UVPolar";
+
+		public override bool displayMaterialInspector => true;
+
+	}
+}
