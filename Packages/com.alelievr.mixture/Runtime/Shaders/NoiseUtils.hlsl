943eef0daeb9589f8c2de07e0bf4a935f03403b3
alelievr <antoinel@unity3d.com>
2022-01-27 17:26:49 +0100

    Fix templates

--- Packages/com.alelievr.mixture/Runtime/Shaders/NoiseUtils.hlsl
+++ Packages/com.alelievr.mixture/Runtime/Shaders/NoiseUtils.hlsl
@@ -168,9 +168,9 @@ float4 GenerateNoiseForChannels(v2f_customrendertexture i, int channels, int see
         case 2: // RG
             return float4(GenerateNoise(i, seed), GenerateNoise(i, seed + 42), 0, 1);
         case 3: // RGB
-            return float4(GenerateNoise(i, seed), GenerateNoise(i, seed + 42), GenerateNoise(i, seed - 69), 1);
+            return float4(GenerateNoise(i, seed), GenerateNoise(i, seed + 42), GenerateNoise(i, seed + 426), 1);
         case 4: // RGBA
-            return float4(GenerateNoise(i, seed), GenerateNoise(i, seed + 42), GenerateNoise(i, seed - 69), GenerateNoise(i, seed + 5359));
+            return float4(GenerateNoise(i, seed), GenerateNoise(i, seed + 42), GenerateNoise(i, seed + 426), GenerateNoise(i, seed + 5359));
         default:
             return GenerateNoise(i, seed).rrrr;
     }
